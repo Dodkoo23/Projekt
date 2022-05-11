cdbcffdefbb029fd95f473f8edb50141e5b5d830
Dodkoo23 <92848160+Dodkoo23@users.noreply.github.com>
2022-05-11 09:16:34 +0200

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string> 
+#include <cstdlib> 
+#include <ctime>
+
+using namespace std;
+void rules();
+int main()
+{
+	string MenoHraca;
+	int balanc;
+	int sazka;
+
+
+
+
+
+
+
+	cout << "------------------------";
+	cout << "-----VITAJ V KASINE-----";
+	cout << "------------------------";
+	cout << "Zadaj svoje hracie meno: "
+		getline(cin, MenoHraca);
+	cout << "Vloz peniaze (€): ";
+		cin >> balanc;
+		do
+		{
+			system("cls");
+			rules();
+			cout << " Tvoj aktualny balanc je " << balanc 
+			do
+				
+				cout << " Vitaj " << MenoHraca << ", Zadaj stavku(";
+					cin >> 
+
+
+		
