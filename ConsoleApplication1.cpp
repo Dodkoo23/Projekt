fbb209a33bb01d60aacc48fdc91cc3d8d40715e7
Dodkoo23 <92848160+Dodkoo23@users.noreply.github.com>
2022-06-08 08:46:32 +0200

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string> 
+#include <cstdlib> 
+#include <ctime>
+
+using namespace std;
+void rules();
+int main()
+{
+	
+string MenoHraca;
+int vklad;
+int sazka;
+bool flag = 0;
+
+
+
+
+
+
+
+	cout << "--------------------------------------------------------------------------------------";
+	cout << "------------------------------------WELCOME IN---------------------------------------";
+	cout << "--------------------------------------------------------------------------------------";
+	cout << "\n==========================================================================================";
+cout << "\n   CCCCC       A        SSSSSSSSS   IIIIIIIII  NN     NN    OOOOOOOO     "
+cout << "\n  CC          A  A      SS             III     NN N   NN  OO       OO    "
+cout << "\n CC          A    A     SSSSSSSSS      III     NN  N  NN  OO       OO    "
+cout << "\n  CC        AAAAAAAA           SS      III     NN   N NN  OO       OO   "
+cout << "\n    CCCCC  A        A   SSSSSSSSS   IIIIIIIII  NN     NN    OOOOOOOO   "
+cout << "\n ===============================================================================================\n";
+
+	cout << "Zadaj svoje hracie meno: ";
+	getline(cin, MenoHraca);
+	cout << "Vloz peniaze s ktorymi chces hrat (€): ";
+	cin >> vklad;
+	do
+	{
+		system("cls");
+		rules();
+		cout << " Tvoj aktualny balanc je " << (vklad)
+
+
+			cout << " Vitaj " << MenoHraca << ", Zadaj stavku";
+		cin >> sazka
+			if (sazka > vklad)
+				cout << "Nemas dostatocny balanc";
+
+	
